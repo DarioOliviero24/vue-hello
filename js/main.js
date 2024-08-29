65b07a1ea5cdfce2a675bfa00dd6791be61be515
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-08-29 10:02:06 +0200

    Hello Vue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+//console.log('ciao');
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+    return {
+        message: 'Hello Vue!'
+    }
+}
+}).mount('#app')
